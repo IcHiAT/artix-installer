2fa583895a0f114ebd995ac72e81b5ab75ddcafb
Chris <103767542+IcHiAT@users.noreply.github.com>
2023-03-09 13:02:43 +0100

    Update installer.sh

--- src/installer.sh
+++ src/installer.sh
@@ -83,6 +83,11 @@ case $(grep vendor /proc/cpuinfo) in
 esac
 
 # Install base system and kernel
-basestrap /mnt base base-devel "$MY_INIT" elogind-"$MY_INIT" "$fs_pkgs" efibootmgr grub "$ucode" dhcpcd wpa_supplicant connman-"$MY_INIT" nano dialog
-basestrap /mnt linux linux-firmware linux-headers mkinitcpio
+basestrap /mnt base base-devel "$MY_INIT" elogind-"$MY_INIT" "$fs_pkgs" efibootmgr grub "$ucode" dhcpcd wpa_supplicant connman-"$MY_INIT" nano dialog 
+basestrap /mnt linux linux-firmware linux-headers mkinitcpio dosfstools
+
+if [ "$MY_INIT" = "openrc" ]; then
+	basestrap /mnt bluez bluez-openrc bluez-utils cups cups-openrc
+fi
+
 fstabgen -U /mnt >/mnt/etc/fstab
